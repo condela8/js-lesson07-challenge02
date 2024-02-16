1da6de78358334f838ddfac92d0114234ccbb610
conde_adra <condeadramane82@gmail.com>
2024-02-15 20:18:58 -0500

    Creating Change Event Listeners

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ var subDurationElement=document.querSelector("#months");
 var result=document.querSelector(".result");
 var subType="basic";
 var subDuration=1;
+
+//Creating Change Event Listeners
+subTypeElement.addEventListener("change", function(e){
+
+    subType = e.target.value;
+    console.log*(subType);
+
+} );
