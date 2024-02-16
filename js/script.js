ced0854092718127c7dab80112d06000c17682fa
conde_adra <condeadramane82@gmail.com>
2024-02-15 20:08:40 -0500

    Selecting elements

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // Your JS code here
+//Selecting Elements
+
+var subTypeElement=document.querySelector("#subscription");
+var subDurationElement=document.querSelector("#months");
+var result=document.querSelector(".result");
+var subType="basic";
+var subDuration=1;
