c39971f80e7f122b609fb806c344304b5f4ea970
conde_adra <condeadramane82@gmail.com>
2024-02-15 20:40:27 -0500

    Using a Function to Calculate the Cost

--- js/script.js
+++ js/script.js
@@ -11,6 +11,25 @@ var subDuration=1;
 subTypeElement.addEventListener("change", function(e){
 
     subType = e.target.value;
-    console.log*(subType);
+    updateSubscriptionDiv();
 
 } );
+subDurationElement.addEventListener("change", function(e){
+    subDuration=Number(e.target.value);
+    updateSubscriptionDiv();
+});
+//Using a Function to Calculate the Cost
+
+var  updateSubscriptionDiv= function(){
+    var monthlyCost= 5;
+    if (subType==="standard"){
+        monthlyCost= 7;
+
+    }
+    else if (subType==="premium"){
+        monthlyCost= 10;
+    }
+    var total= subDuration*monthlyCost;
+    result.innerText=`You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+
+}
